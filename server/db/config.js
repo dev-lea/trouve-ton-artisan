5a3485fdda0ed49d44219a21a7f311be1f0eecdf
DevLea <dev.lea2b@gmail.com>
2025-10-02 15:08:12 +0200

    fix: set utf8mb4 charset in sequelize config

--- server/db/config.js
+++ server/db/config.js
@@ -11,5 +11,12 @@ export const sequelize = new Sequelize(
     host: process.env.DB_HOST || "localhost",
     dialect: "mysql",
     logging: false,
+    dialectOptions: {
+      charset: "utf8mb4",   
+    },
+    define: {
+      charset: "utf8mb4",
+      collate: "utf8mb4_unicode_ci",
+    },
   }
 );
